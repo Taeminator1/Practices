c30a2e21f2d392ace86a39334cd9aa412266f9f3
Taemin <dbsxoals689@naver.com>
2021-12-06 03:42:55 +0900

    RxCocoa 적용해서, 입력에 대해 Table View에 대한 까지 변경되도록 수정

--- RxSwift_In_4_Hours/Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
+++ RxSwift_In_4_Hours/Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
@@ -11,16 +11,39 @@ import RxSwift
 
 class MenuListViewModel {
     
-    let menus: [Menu] = [
-        Menu(name: "메뉴1", price: 300, count: 1),
-        Menu(name: "메뉴2", price: 400, count: 4),
-        Menu(name: "메뉴3", price: 200, count: 3),
-        Menu(name: "메뉴4", price: 100, count: 2),
-    ]
-    
     // Observable은 데이터를 subscribe할 수 있지만, 데이터를 변경할 수 없음
     // Observable은 값을 넘겨줄 뿐, 외부에서 받아서 넘겨줄 수 없음
     // Subject는 Observable 밖에서 데이터를 조작해서 새로운 데이터를 집어 넣는 것이 가능
-    var itemsCount: PublishSubject<Int> = PublishSubject()
-    var totalPrice: PublishSubject<Int> = PublishSubject()
+    var menuObservable = BehaviorSubject<[Menu]>(value: [])
+    
+    lazy var itemsCount = menuObservable.map {
+        $0.map { $0.count }.reduce(0) { $0 + $1 }
+    }
+    lazy var totalPrice = menuObservable.map {
+        $0.map { $0.price * $0.count }.reduce(0) { $0 + $1 }
+    }
+    
+    init() {
+        let menus: [Menu] = [
+            Menu(name: "메뉴1", price: 300, count: 1),
+            Menu(name: "메뉴2", price: 400, count: 4),
+            Menu(name: "메뉴3", price: 200, count: 3),
+            Menu(name: "메뉴4", price: 100, count: 2),
+        ]
+        
+        menuObservable.onNext(menus)
+    }
+    
+    func clearAllItemSelections() {
+        _ = menuObservable
+            .map { menus in
+                menus.map { m in
+                    Menu(name: m.name, price: m.price, count: 0)
+                }
+            }
+            .take(1)                        // 한 번만 실행시키고 끝나게 한다.
+            .subscribe(onNext: {
+                self.menuObservable.onNext($0)
+            })
+    }
 }
